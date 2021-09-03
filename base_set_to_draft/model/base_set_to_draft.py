edef2ce700d60c2880ac6f7a04c262160c75eedf
amirul <muhammad.amirul.muk@gmail.com>
2021-09-03 16:22:47 +0700

    [UPDATE] simplify method

--- base_set_to_draft/model/base_set_to_draft.py
+++ base_set_to_draft/model/base_set_to_draft.py
@@ -12,12 +12,8 @@ class BaseSetToDraft(models.AbstractModel):
     def set_to_draft(self):
         self.write({'state': 'draft'})
 
-    def fields_view_get(
-            self, view_id=None, view_type="form", toolbar=False, submenu=False
-    ):
-        res = super().fields_view_get(
-            view_id=view_id, view_type=view_type, toolbar=toolbar, submenu=submenu
-        )
+    def fields_view_get(self, view_id=None, view_type="form", toolbar=False, submenu=False):
+        res = super().fields_view_get(view_id=view_id, view_type=view_type, toolbar=toolbar, submenu=submenu)
         if view_type == "form" and not self._button_draft_exist and res['fields'].get('state'):
             doc = etree.XML(res["arch"])
             for node in doc.xpath(self._button_xpath):
@@ -26,13 +22,8 @@ class BaseSetToDraft(models.AbstractModel):
                 )
                 new_node = etree.fromstring(str_element)
                 for new_element in new_node:
+                    # add new button before the first button (if exist)
                     node.addprevious(new_element)
 
-            # Override context for postprocessing
-            View = self.env["ir.ui.view"]
-            if view_id and res.get("base_model", self._name) != self._name:
-                View = View.with_context(base_model_name=res["base_model"])
-            new_arch, new_fields = View.postprocess_and_fields(doc, self._name)
-            res["arch"] = new_arch
-            res["fields"] = new_fields
+            res['arch'] = etree.tostring(doc, encoding='unicode')
         return res
